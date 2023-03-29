a780c91258b171711b6ec0d8e153a955df5cdd57
eddyangelop <eddysoullive@gmail.com>
2023-03-29 19:09:33 -0300

    implementado projeto infrastructure

---
+++ Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,40 @@
+﻿using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(String.Empty);
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        //private string GetStringConnectionConfing()
+        //{
+
+
+
+
+          //  string strCon = "Data Source=LAPTOP-N3KBHKL3;Initial Catalog=DDD_ECOMMERCE;Integrated Security=False;User ID=eddyangelop;Password=Developer@123;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+           // return strCon;
+        //}
+
+    }
+}
